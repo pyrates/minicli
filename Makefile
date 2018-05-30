3a5e9b4f5a8bf3cfd36fe0efb15f214ed761d951
Yohan Boniface <yb@enix.org>
2018-05-30 21:18:47 +0200

    0.4.1 with correct markdown description on pypi
    For this:
    
    - make sure to add long_description_content_type='text/markdown'
      on your setup
    - make sure to use latest setuptools package
    - make sure to use latest wheel package
    - make sure to upload with twine (not setup.py) and with latest
      twine package
    
    More: https://dustingram.com/articles/2018/03/16/markdown-descriptions-on-pypi

---
+++ Makefile
@@ -0,0 +1,8 @@
+develop:
+	python setup.py develop
+dist:
+	python setup.py sdist bdist_wheel
+upload:
+	twine upload dist/*
+clean:
+	rm -rf *.egg-info/ dist/ build/
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-VERSION = (0, 4, 0)
+VERSION = (0, 4, 1)
 
 __author__ = 'Pyrates Team'
 __contact__ = "yohan.boniface@data.gouv.fr"
